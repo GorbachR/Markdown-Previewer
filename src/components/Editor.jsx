4d3411ad2adec64e7e768fc449226a9a59b49555
GorbachR <raphael_gorbach@gmx.at>
2022-11-22 03:29:23 +0100

    Style:
    fix flex item overflow issue (set flex item min-width: 0,
    surround parsed table with overflow wrapper, add overflow
    scroll-bar to pre, ol, ul, .table-wrapper), adjust typography,
    improve mobile view, move scss partials

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -10,14 +10,7 @@ function Editor({ markdown, setMarkdown }) {
       <label htmlFor="editor">
         <h2>Markdown Editor</h2>
       </label>
-      <textarea
-        name="markdown"
-        id="editor"
-        cols="50"
-        rows="20"
-        value={markdown}
-        onChange={onMarkdownChange}
-      ></textarea>
+      <textarea name="markdown" id="editor" value={markdown} onChange={onMarkdownChange}></textarea>
     </div>
   );
 }
--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { marked } from "marked";
 import DOMPurify from "dompurify";
 import "./styles/Preview.scss";
 
 function Preview({ markdown }) {
   const htmlString = DOMPurify.sanitize(marked.parse(markdown));
+
+  useEffect(() => {
+    document.querySelectorAll(".preview > table").forEach((element) => {
+      const wrapper = document.createElement("div");
+      wrapper.classList.add("table-wrapper");
+      element.before(wrapper);
+      wrapper.append(element);
+    });
+
+    return () => {};
+  }, [htmlString]);
   return (
     <div className="preview-container">
       <h2>Output</h2>
